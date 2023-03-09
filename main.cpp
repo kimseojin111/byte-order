1178e316c017db776608c6fb42ad2cfe0180d791
seojin <palmtreejin@gmail.com>
2023-03-09 22:06:44 +0900

    byte order sujung

--- main.cpp
+++ main.cpp
@@ -20,17 +20,30 @@ void write_4660() {
 	dump(&port, sizeof(port));
 }
 
+
+uint16_t ntohs(uint16_t x){
+	return (x>>8)|(x<<8);
+}
+
+
+uint32_t ntohl(uint32_t x){
+	return (x<<24)|(x<<8)&(0x00ff0000)|(x>>8)&(0x0000ff00)|(x>>24);
+}
+
+
+
 void  write_0x1234() {
 	uint8_t network_buffer[] = { 0x12, 0x34 };
 	uint16_t* p = reinterpret_cast<uint16_t*>(network_buffer);
-	uint16_t n = *p; // TODO
+	uint16_t n = ntohs(*p); // TODO
 	printf("16 bit number=0x%x\n", n);
 }
 
 void  write_0x12345678() {
 	uint8_t network_buffer[] = { 0x12, 0x34, 0x56, 0x78 };
 	uint32_t* p = reinterpret_cast<uint32_t*>(network_buffer);
-	uint32_t n = *p; // TODO
+	uint32_t n = ntohl(*p); // TODO
+	//uint32_t n = *p;
 	printf("32 bit number=0x%x\n", n);
 }
 
